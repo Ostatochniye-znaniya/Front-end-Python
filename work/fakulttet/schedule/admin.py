090d9a71ea5e581f0aa52916d2f54e6967bed3f6
MilanShemet <boycrazy0776@gmail.com>
2026-03-02 13:45:30 +0300

    Delete work/fakulttet directory

--- work/fakulttet/schedule/admin.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Кафедра)
-admin.site.register(Группа)
-admin.site.register(Дисциплина)
-admin.site.register(Преподаватель)
-admin.site.register(ПроверкаЗнаний)
--- work/fakulttet/schedule/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from .views import скачать_pdf
-
-urlpatterns = [
-    path('pdf/', скачать_pdf, name='скачать_pdf'),
-]
