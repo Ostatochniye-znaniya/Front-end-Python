9594bb1a66e685b972ac6c649e3aa5b2f2845cff
MilanShemet <boycrazy0776@gmail.com>
2026-03-02 13:44:45 +0300

    Add files via upload

---
+++ work/fakulttet/schedule/admin.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Кафедра)
+admin.site.register(Группа)
+admin.site.register(Дисциплина)
+admin.site.register(Преподаватель)
+admin.site.register(ПроверкаЗнаний)
